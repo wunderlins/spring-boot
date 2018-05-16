510d51406f55b5f47b4389fee0336afbe51e107f
Simon Wunderlin <swunderlin@gmail.com>
2018-05-16 20:12:11 +0200

    added

---
+++ starter-rest-jpa2/src/main/java/hello/Person.java
@@ -0,0 +1,37 @@
+package hello;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name="firstname")
+	private String firstName;
+	
+	@Column(name="lastname")
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
---
+++ starter-rest-jpa2/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into person (id, firstname, lastname) values (1, 'simon', 'w');
